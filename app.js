5e07dfae299af5191ff33187f897ebbd17d0fe15
Lee Yongjin <greattoe@gmail.com>
2020-12-16 16:19:26 +0900

    On branch main  Changes to be committed: 	new file:   app.js 	new file:   code4arduino/code4arduino.ino 	new file:   public/html/ver1/index.html 	new file:   public/html/ver2/index.html 	new file:   public/html/ver3/index.html 	new file:   public/images/offBlue.gif 	new file:   public/images/offGreen.gif 	new file:   public/images/offRed.gif 	new file:   public/images/offYellow.gif 	new file:   public/images/onBlue.gif 	new file:   public/images/onGreen.gif 	new file:   public/images/onRed.gif 	new file:   public/images/onYellow.gif 	new file:   public/index1.html     new file:   public/index2.html 	new file:   public/index3.html 	new file:   public/javascripts/annyang.min.js 	new file:   public/javascripts/jquery-1.12.4.min.js 	new file:   public/javascripts/jquery.min.js 	new file:   public/javascripts/responsivevoice.js 	new file:   public/stylesheets/style.css

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const SerialPort = require('serialport');
+const sp = new SerialPort('COM2', {
+  baudRate: 9600
+});
+
+const port = 3000;
+
+app.get('/led_on',function(req,res)
+{
+  sp.write('1\n\r', function(err){
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('send: led on');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('LED On\n');
+  });
+});
+
+app.get('/led_off',function(req,res)
+{
+  sp.write('0\n\r', function(err){
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('send: led off');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('LED Off\n');
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+  console.log('listening on *:' + port);
+});
